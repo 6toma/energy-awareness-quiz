df115cdb4a1b23c57a2db5379d32c1b0ce492807
Robert Kurvits <r.kurvits@student.tudelft.nl>
2022-02-24 12:53:16 +0100

    Implement Activity class

--- commons/src/main/java/commons/Activity.java
+++ commons/src/main/java/commons/Activity.java
@@ -1,7 +1,71 @@
 package commons;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
 public class Activity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private Integer consumption;
+    //private String source;
 
 
+    public Activity(String title, Integer consumption) {
+        this.title = title;
+        this.consumption = consumption;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getConsumption() {
+        return consumption;
+    }
+
+    public void setConsumption(Integer consumption) {
+        this.consumption = consumption;
+    }
+
+    @Override
+    public String toString() {
+        return "Activity{" +
+            "id=" + id +
+            ", title='" + title + '\'' +
+            ", consumption=" + consumption +
+            '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Activity activity = (Activity) o;
+        return Objects.equals(id, activity.id) && Objects.equals(title, activity.title) && Objects.equals(consumption, activity.consumption);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, consumption);
+    }
 }
--- server/src/main/java/server/api/ActivityController.java
+++ server/src/main/java/server/api/ActivityController.java
@@ -24,4 +24,6 @@ public class ActivityController {
         this.repo = repo;
     }
 
+
+
 }
