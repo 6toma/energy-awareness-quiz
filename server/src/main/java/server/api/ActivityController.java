286e28edbe5cdc1fbe2e8ad8fe44e2ce46140d72
Timur Ildarovich Mukminov <T.I.Mukminov@student.tudelft.nl>
2022-02-24 18:25:51 +0100

    Added endpoint and updated compatibilty with latest java version
    Added getRandomQuestion endpoint. Through some unkown changes the buidl filoes changed too

--- server/src/main/java/server/api/ActivityController.java
+++ server/src/main/java/server/api/ActivityController.java
@@ -3,10 +3,12 @@ package server.api;
 import commons.Activity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
 import server.database.ActivityRepository;
 
 import java.util.Random;
@@ -39,8 +41,8 @@ public class ActivityController {
             return ResponseEntity.badRequest().build();
         }
 
-        // activity.save() or something like that should be here when we connect it to the database
-        return ResponseEntity.ok(activity); // returns the same object if everything ok
+        Activity act = repo.save(activity); // saves activity to the database
+        return ResponseEntity.ok(act); // returns the same object if everything ok
     }
 
     private static boolean isNullOrEmpty(Object o) { // checks if an object is null or empty
@@ -52,6 +54,14 @@ public class ActivityController {
             return s.isEmpty();
         }
         return false;
+    }
+
+    @GetMapping("/random")
+    public Activity getRandomActivity(){
+        if (repo.count()==0){ // checks if the repository is empty
+            return null;
+        }
+        return repo.getById((long)random.nextInt((int)repo.count())); // Selects a random question by id between 0 and size of repo exclusive
     }
 
 }
