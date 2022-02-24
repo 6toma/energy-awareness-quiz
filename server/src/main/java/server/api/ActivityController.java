93eaf1bd89fd29ca7e04ac0b1d9a3aa5ea740c96
Zofia Rogacka-Trojak <z.rogacka-trojak@student.tudelft.nl>
2022-02-24 23:57:58 +0100

    Adding get activity by id endpoint

--- server/src/main/java/server/api/ActivityController.java
+++ server/src/main/java/server/api/ActivityController.java
@@ -3,10 +3,7 @@ package server.api;
 import commons.Activity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import server.database.ActivityRepository;
 
 import java.util.Random;
@@ -28,26 +25,34 @@ public class ActivityController {
         this.repo = repo;
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Activity> getActivityById(@PathVariable("id") long id) {
+        if (id < 0 || !repo.existsById(id)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(repo.findById(id).get());
+    }
+
     @PostMapping("/")
-    public ResponseEntity<Activity> addActivity(@RequestBody Activity activity){
+    public ResponseEntity<Activity> addActivity(@RequestBody Activity activity) {
 
         // checks if the json is of a proper activity
-        if(activity == null || isNullOrEmpty(activity.getTitle())
-                || isNullOrEmpty(activity.getConsumption_in_wh())
-                || isNullOrEmpty(activity.getSource())){
+        if (activity == null || isNullOrEmpty(activity.getTitle())
+            || isNullOrEmpty(activity.getConsumption_in_wh())
+            || isNullOrEmpty(activity.getSource())) {
 
             return ResponseEntity.badRequest().build();
         }
 
-        // activity.save() or something like that should be here when we connect it to the database
-        return ResponseEntity.ok(activity); // returns the same object if everything ok
+        Activity saved = repo.save(activity);
+        return ResponseEntity.ok(saved); // returns the same object if everything ok
     }
 
     private static boolean isNullOrEmpty(Object o) { // checks if an object is null or empty
-        if(o == null){
+        if (o == null) {
             return true;
         }
-        if(o instanceof String){
+        if (o instanceof String) {
             String s = (String) o;
             return s.isEmpty();
         }
