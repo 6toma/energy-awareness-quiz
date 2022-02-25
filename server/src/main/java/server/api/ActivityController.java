3a32a496615a555a8ed9515b72887e804b43a397
Stanislaw Howard <S.M.Howard@student.tudelft.nl>
2022-02-25 09:46:10 +0100

    added update endpoint

--- server/src/main/java/server/api/ActivityController.java
+++ server/src/main/java/server/api/ActivityController.java
@@ -3,10 +3,7 @@ package server.api;
 import commons.Activity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import server.database.ActivityRepository;
 
 import java.util.Random;
@@ -38,7 +35,7 @@ public class ActivityController {
 
             return ResponseEntity.badRequest().build();
         }
-
+        repo.save(activity);
         // activity.save() or something like that should be here when we connect it to the database
         return ResponseEntity.ok(activity); // returns the same object if everything ok
     }
@@ -52,6 +49,27 @@ public class ActivityController {
             return s.isEmpty();
         }
         return false;
+    }
+    /**
+    Api endpoint for updating an Activity in the database by specifying an id in the path
+     */
+    @PostMapping("/update/{id}")
+    public ResponseEntity<Activity> updateActivity(@RequestBody Activity activity, @PathVariable("id") long id) {
+        // check if a id exists in database
+        if (id < 0 || !repo.existsById(id)) {
+            return ResponseEntity.badRequest().build();
+        }
+        // checks if the json is of a proper activity
+        if(activity == null || isNullOrEmpty(activity.getTitle())
+                || isNullOrEmpty(activity.getConsumption_in_wh())
+                || isNullOrEmpty(activity.getSource())){
+
+            return ResponseEntity.badRequest().build();
+        }
+
+        activity.setId(id);  // set the id of the record to be changed
+        repo.save(activity); // update the activity
+        return ResponseEntity.ok(activity);
     }
 
 }
