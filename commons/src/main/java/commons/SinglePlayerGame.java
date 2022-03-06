1d10e79e84d7afccaa6e77557c021eb49f2c0e9a
Stanislaw Howard <S.M.Howard@student.tudelft.nl>
2022-03-06 13:18:16 +0100

    single player game class update

--- commons/src/main/java/commons/SinglePlayerGame.java
+++ commons/src/main/java/commons/SinglePlayerGame.java
@@ -5,38 +5,56 @@ import java.util.Objects;
 
 /**
  * Single player game class
- * Methods and fields regarding a single player game
+ * Used for handling a single-player game
  */
 public class SinglePlayerGame {
 
-    private ArrayList<Activity> activities;
+    private ArrayList<Question> questions;
     private Player player;
-    private int streak = 1; // streak field for getting more points when you answer questions in a row
+    private int streak = 0; // streak field for getting more points when you answer questions correctly in a row
     private int questionNumber = 1;
 
     public SinglePlayerGame() {};
 
-    // to be implemented
+    public SinglePlayerGame(Player player, ArrayList<Question> questions) {
+        this.player = player;
+        this.questions = questions;
+    }
+
+    /**
+     * Not necessarily a good thing
+     * Constructor which generates a set of questions for a game
+     * @param player
+     */
     public SinglePlayerGame(Player player) {
         this.player = player;
         for(int i = 0 ; i < 20; i++){
-//            this.activities.addRandomActivity();
+        // this.questions.addRandomQuestion();
         }
     }
+
+    public void addQuestion(Question question){ questions.add(question); }
 
     /**
      * This method adds points to a score of a player
      * A particular formula for the points has been developed.
      * parsing an int equal to 2137 will be qualified as answering the question wrongly
      * @param timeWhenAnswered time in seconds how long it took a user to answer a question
+     * @param guessQuestionRate for every other question than a guess question this will be set to 1.0
+     *                          for the guess question this will be set to a percentage how good the guess was
      */
-    public void addPoints(int timeWhenAnswered){
+    public void addPoints(int timeWhenAnswered, double guessQuestionRate){
         if(timeWhenAnswered == 2137){
+            resetStreak();
+            nextQuestion();
             return;
         }
+        incrementStreak();
         int currentScore = player.getScore();
-        player.setScore( currentScore + getPointsToBeAdded(timeWhenAnswered) );
+        player.setScore( currentScore + (int)guessQuestionRate * getPointsToBeAdded(timeWhenAnswered) );
+        nextQuestion();
     }
+
 
     /**
      * The points achievable for a single question are from 950 to 1050 depending on the number of seconds it took the user to select an answer
@@ -52,27 +70,26 @@ public class SinglePlayerGame {
     }
 
     /**
-     * increments the question counter
+     * Methods for manipulating streak and question numbers
      */
-    public void nextQuestion() {
-        questionNumber++;
-    }
+    public void nextQuestion(){ questionNumber++;}
+
+    public void resetStreak() { streak = 0;}
+
+    public void incrementStreak() { streak++;}
+
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof SinglePlayerGame)) return false;
         SinglePlayerGame that = (SinglePlayerGame) o;
-        return getStreak() == that.getStreak() && getQuestionNumber() == that.getQuestionNumber() && Objects.equals(getActivities(), that.getActivities()) && Objects.equals(getPlayer(), that.getPlayer());
+        return getStreak() == that.getStreak() && getQuestionNumber() == that.getQuestionNumber() && Objects.equals(questions, that.questions) && Objects.equals(getPlayer(), that.getPlayer());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getActivities(), getPlayer(), getStreak(), getQuestionNumber());
-    }
-
-    public ArrayList<Activity> getActivities() {
-        return activities;
+        return Objects.hash(questions, getPlayer(), getStreak(), getQuestionNumber());
     }
 
     public Player getPlayer() {
@@ -83,13 +100,15 @@ public class SinglePlayerGame {
         return streak;
     }
 
-    public int getQuestionNumber() {
-        return questionNumber;
+    public ArrayList<Question> getQuestions() {
+        return questions;
     }
 
-    public void setActivities(ArrayList<Activity> activities) {
-        this.activities = activities;
+    public void setQuestions(ArrayList<Question> questions) {
+        this.questions = questions;
     }
+
+    public int getQuestionNumber() { return questionNumber; }
 
     public void setPlayer(Player player) {
         this.player = player;
@@ -102,4 +121,5 @@ public class SinglePlayerGame {
     public void setQuestionNumber(int questionNumber) {
         this.questionNumber = questionNumber;
     }
+
 }
--- commons/src/test/java/commons/SinglePlayerGameTest.java
+++ commons/src/test/java/commons/SinglePlayerGameTest.java
@@ -1,4 +1,15 @@
 package commons;
 
+import org.junit.jupiter.api.BeforeEach;
+
+
 public class SinglePlayerGameTest {
+
+    SinglePlayerGame singlePlayerGame = new SinglePlayerGame();
+
+    @BeforeEach
+    public void setup(){
+
+    }
+
 }
