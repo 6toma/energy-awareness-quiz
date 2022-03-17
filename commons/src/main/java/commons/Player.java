6c5dda3a4e93eedf51a449b7f3890957010df3be
Robert Kurvits <r.kurvits@student.tudelft.nl>
2022-03-17 12:18:57 +0100

    Refactor to use lombok for boilerplate code

--- commons/src/main/java/commons/Player.java
+++ commons/src/main/java/commons/Player.java
@@ -1,16 +1,18 @@
 package commons;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.util.Objects;
 
 /**
  * Activity class that is stored in the database
  *
  * Stores an activities
  */
+@Data
 @Entity
 public class Player {
 
@@ -36,52 +38,6 @@ public class Player {
     public Player(Long id, String name, Integer score) {
         this.id = id;
         this.name = name;
-        this.score = score;
-    }
-
-    @Override
-    public String toString() {
-        return "Player{" +
-                "ID=" + id + '\'' +
-                "name='" + name + '\'' +
-                ", score=" + score +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Player)) return false;
-        Player player = (Player) o;
-        return Objects.equals(name, player.name) && Objects.equals(score, player.score) && Objects.equals(id, player.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, score);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getScore() {
-        return score;
-    }
-
-    public void setScore(Integer score) {
         this.score = score;
     }
 }
