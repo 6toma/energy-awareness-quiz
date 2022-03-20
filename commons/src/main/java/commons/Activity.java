ccca2b162af4d388e09775cc7fdbc2d6f6e55197
Robert Kurvits <r.kurvits@student.tudelft.nl>
2022-03-20 18:41:57 +0100

    Fix problems with image datatype

--- commons/src/main/java/commons/Activity.java
+++ commons/src/main/java/commons/Activity.java
@@ -7,8 +7,8 @@ import javax.imageio.ImageIO;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Transient;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 
@@ -27,9 +27,9 @@ public class Activity {
     private String title;
     private Long consumption_in_wh;
     private String source;
-    @ToString.Exclude
-    @Transient
-    private BufferedImage image;
+    @ToString.Exclude // Image is not included in the toString method (would make it too long)
+    @Transient // Image is not stored in the database
+    private byte[] image; // image is a byte array because it's a pretty efficient way to send it (I hope)
 
     // needed for creating object from JSON
     public Activity() {
@@ -57,12 +57,15 @@ public class Activity {
      * Sets image to null if no image was found
      */
     public void initializeImage(){
-        BufferedImage img;
         try{
-            img = ImageIO.read(new File("./server/src/main/resources/activity-bank-pictures/" + this.image_path));
-            this.image = img;
-        } catch (IOException e){
-            e.printStackTrace();
+            // Reads the image from the file to a BufferedImage
+            BufferedImage img = ImageIO.read(new File("./server/src/main/resources/activity-bank-pictures/" + this.image_path));
+            // Creates a new ByteArrayOutputStream
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            // Writes the image to the output stream. Sending the image gives an error (because it's inefficient and shouldn't be done)
+            ImageIO.write(img, "png", outputStream); // jpg would be faster but png supports transparency
+            this.image = outputStream.toByteArray();
+        } catch (IOException | IllegalArgumentException e){ // Catches an error if an image couldn't be found
             System.err.println("Couldn't find picture");
             this.image = null;
         }
