6d5b116242ef019c1466e9eb60c6b8ce005aabe6
Stanislaw Howard <S.M.Howard@student.tudelft.nl>
2022-03-21 15:32:56 +0100

    added lombok and javadoc to EtimationQuestion class

--- commons/src/main/java/commons/EstimationQuestion.java
+++ commons/src/main/java/commons/EstimationQuestion.java
@@ -1,16 +1,28 @@
 package commons;
 
 import java.util.List;
-import java.util.Objects;
 
+import lombok.*;
+
+@Data
+/**
+ * Class for estimation questions. A player is supposed to estimate which question uses similar
+ * amount of energy
+ */
 public class EstimationQuestion extends Question {
-
     private List<Activity> activities; // list of activities
     private Long correct_answer; // index of the correct answer in the list
 
+    /**
+     * no args constructor
+     */
     public EstimationQuestion() {
     }
 
+    /**
+     * constructor with the activities
+     * @param activities list of 3 activities to chose from
+     */
     public EstimationQuestion(List<Activity> activities) {
         this.activities = activities;
         this.correct_answer = generateCorrectAnswer();
@@ -22,38 +34,5 @@ public class EstimationQuestion extends Question {
         }
         Activity current = activities.get(0);
         return current.getConsumption_in_wh();
-    }
-
-    @Override
-    public String toString() {
-        return "EstimationQuestion{" +
-                "activity " + activities +
-                ", correct_answer=" + correct_answer +
-                "}";
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        EstimationQuestion that = (EstimationQuestion) o;
-        return correct_answer == that.correct_answer && Objects.equals(activities, that.activities);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(activities, correct_answer);
-    }
-
-    public List<Activity> getActivities() {
-        return activities;
-    }
-
-    public void setActivities(List<Activity> activities) {
-        this.activities = activities;
-    }
-
-    public Long getCorrect_answer() { // no setter for this because it's generated
-        return correct_answer;
     }
 }
