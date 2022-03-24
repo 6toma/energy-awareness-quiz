6e12b8e9d94f19bfad0480c75bde0b0ed2249827
Timur Ildarovich Mukminov <T.I.Mukminov@student.tudelft.nl>
2022-03-24 17:05:33 +0100

    Added a way for server to respond to client
    Endpoint where server can send integer of what has changed.

--- server/src/main/java/server/api/LongPollController.java
+++ server/src/main/java/server/api/LongPollController.java
@@ -4,10 +4,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
 
 
 @RestController
@@ -28,6 +32,36 @@ public class LongPollController {
     }
      */
 
+    private Map<Object, Consumer<Integer>> listeners = new HashMap<>();
+
+    /**
+     * Gets a number that corresponds to what has changed
+     * @return
+     */
+    @GetMapping("/update")
+    public DeferredResult<ResponseEntity<Integer>> getUpdate(){
+        var noContent = ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        var res = new DeferredResult<ResponseEntity<Integer>>(5000L,noContent);
+        var key = new Object();
+        listeners.put(key,c ->{
+            res.setResult(ResponseEntity.ok(c));
+        });
+        res.onCompletion(()-> {
+            listeners.remove(key);
+        });
+        return res;
+    }
+
+    /**
+     *
+     * @param change
+     * @return
+     */
+    @PostMapping(path = {"add-update"})
+    public ResponseEntity<Integer> giveUpdate(Integer change){
+        listeners.forEach((k,l) -> l.accept(change));
+        return ResponseEntity.ok(change);
+    }
 
     /**
      * Cool piece of code found, it redirects so it keeps polling
@@ -38,6 +72,7 @@ public class LongPollController {
     private ResponseEntity<List<Integer>> keepPolling(Integer input) throws InterruptedException {
         Thread.sleep(5000);
         HttpHeaders headers = new HttpHeaders();
+        //"/getMessages?id=" + input.getID() + "&type=" + input.getType()
         headers.setLocation(URI.create("/getMessages?id=" + input + "&type=" + input));
         return new ResponseEntity<>(headers, HttpStatus.TEMPORARY_REDIRECT);
     }
